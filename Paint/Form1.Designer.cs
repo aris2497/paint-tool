54d9a16fe0e68f8e2e9cbad67e98baa962b786e6
aris2497 <pk3630v@greenwich.ac.uk>
2021-04-21 19:38:52 +0100

    initial commit

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.erase_btn = new System.Windows.Forms.Button();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.clear_btn);
+            this.panel1.Controls.Add(this.erase_btn);
+            this.panel1.Controls.Add(this.start_btn);
+            this.panel1.Controls.Add(this.stop_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // erase_btn
+            // 
+            this.erase_btn.Location = new System.Drawing.Point(559, 0);
+            this.erase_btn.Name = "erase_btn";
+            this.erase_btn.Size = new System.Drawing.Size(75, 45);
+            this.erase_btn.TabIndex = 2;
+            this.erase_btn.Text = "Erase";
+            this.erase_btn.UseVisualStyleBackColor = true;
+            this.erase_btn.Click += new System.EventHandler(this.erase_btn_Click);
+            // 
+            // start_btn
+            // 
+            this.start_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.start_btn.Location = new System.Drawing.Point(631, 0);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(81, 45);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(709, 0);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(91, 45);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 45);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 405);
+            this.canvas.TabIndex = 1;
+            
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(451, 11);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(75, 23);
+            this.clear_btn.TabIndex = 3;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button erase_btn;
+        private System.Windows.Forms.Button clear_btn;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Pen MyPen;
+        Graphics g = null;
+
+        int x = -1;
+        int y = -1;
+
+        private bool pressed = false;
+        private bool draw = false;
+        private bool erase = false;
+
+        //erasing tools
+        private Rectangle rect;
+        private Size size = new Size(5, 5);
+
+        public Form1()
+        {
+            InitializeComponent();
+            MyPen = new Pen(Color.Red, 5);
+            g = canvas.CreateGraphics(); //creates the Graphics for the Control
+
+        }
+
+        private void erase_btn_Click(object sender, EventArgs e)
+        {
+            erase = true;
+            draw = false;
+
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            draw = false;
+            erase = false;
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            draw = true;
+
+            erase = false;
+        }
+
+        private void canvas_MouseClick(object sender, MouseEventArgs e) //ocurs when the mouse button is realised
+        {
+            pressed = false;
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e) //occurs when the mouse button is pressed
+        {
+            pressed = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e) //occurs when the mouse is moving
+        {
+            rect = new Rectangle(e.Location, size);
+
+            if (pressed & draw)
+            {
+                g.SmoothingMode =
+                System.Drawing.Drawing2D.SmoothingMode.AntiAlias; //smoothing the lines
+                g.DrawLine(MyPen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+            else if (erase & pressed)
+            {
+                canvas.Invalidate(rect, false);
+            }
+        }
+
+        private void DrawPointer(Rectangle rect)
+        {
+            g.DrawRectangle(MyPen, rect);
+
+        }
+
+        private void DisplayEraser(Rectangle rect)
+        {
+            g.DrawRectangle(MyPen, rect); //area to erase
+            
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            canvas.Invalidate(); //clear the canvas
+        }
+    }
+}
